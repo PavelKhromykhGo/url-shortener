5db0dab0c0d36aa26047a85e0cf2e85cd55790ee
PavelKhromykh <drykilllogic232a@gmail.com>
2025-11-21 02:12:35 +0300

    add config

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/PavelKhromykhGo/url-shortener
 
 go 1.24
+
+require go.uber.org/zap v1.27.1
+
+require go.uber.org/multierr v1.10.0 // indirect
---
+++ internal/config/config.go
@@ -0,0 +1,83 @@
+package config
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Config struct {
+	Env              string
+	HTTPAddr         string
+	PostgresDNS      string
+	RedisAddr        string
+	RedisDB          int
+	RedisPassword    string
+	KafkaBrokers     []string
+	KafkaClicksTopic string
+	BaseURL          string
+}
+
+func Load() (*Config, error) {
+	cfg := &Config{
+		Env:              getEnv("APP_ENV", "dev"),
+		HTTPAddr:         getEnv("HTTP_ADDR", ":8080"),
+		PostgresDNS:      getEnv("POSTGRES_DNS", ""),
+		RedisAddr:        getEnv("REDIS_ADDR", "localhost:6379"),
+		RedisDB:          getEnvInt("REDIS_DB", 0),
+		RedisPassword:    getEnv("REDIS_PASSWORD", ""),
+		KafkaBrokers:     splitComma(getEnv("KAFKA_BROKERS", "localhost:9092")),
+		KafkaClicksTopic: getEnv("KAFKA_CLICKS_TOPIC", "clicks"),
+		BaseURL:          getEnv("BASE_URL", "http://localhost:8080"),
+	}
+
+	if cfg.PostgresDNS == "" {
+		return nil, fmt.Errorf("POSTGRES_DNS is required")
+	}
+	if len(cfg.KafkaBrokers) == 0 {
+		return nil, fmt.Errorf("KAFKA_BROKERS is required")
+	}
+	if cfg.BaseURL == "" {
+		return nil, fmt.Errorf("BASE_URL is required")
+	}
+
+	return cfg, nil
+}
+
+func getEnv(key, def string) string {
+	if val, ok := os.LookupEnv(key); ok {
+		return val
+	}
+	return def
+}
+
+func getEnvInt(key string, def int) int {
+	valStr, ok := os.LookupEnv(key)
+	if !ok || valStr == "" {
+		return def
+	}
+
+	v, err := strconv.Atoi(valStr)
+	if err != nil {
+		return def
+	}
+	return v
+}
+
+func splitComma(s string) []string {
+	if s == "" {
+		return nil
+	}
+
+	parts := strings.Split(s, ",")
+	out := make([]string, 0, len(parts))
+
+	for _, p := range parts {
+		v := strings.TrimSpace(p)
+		if v != "" {
+			out = append(out, v)
+		}
+	}
+	return out
+}
