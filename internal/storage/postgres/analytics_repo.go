012ff083d9a6bf00547286c811016506379e8112
PavelKhromykh <drykilllogic232a@gmail.com>
2025-12-03 23:48:08 +0300

    add analytics

--- internal/storage/postgres/analytics_repo.go
+++ internal/storage/postgres/analytics_repo.go
@@ -32,6 +32,12 @@ DO UPDATE SET
     count = click_stats_daily.count + 1,
     updated_at = now()
 `
+const getClickEventQuery = `
+SELECT date, count
+FROM click_stats_daily
+WHERE link_id = $1 AND date >= $2 AND date <= $3
+ORDER BY date
+`
 
 func (r *AnalyticsRepository) InsertClickEvent(ctx context.Context, event kafka.ClickEvent) error {
 	_, err := r.pool.Exec(ctx, insertClickEventQuery,
@@ -51,4 +57,31 @@ func (r *AnalyticsRepository) IncrementDailyStat(ctx context.Context, linkID int
 		date,
 	)
 	return err
+}
+
+func (r *AnalyticsRepository) GetDailyStats(ctx context.Context, linkID int64, from, to time.Time) ([]analytics.DailyStat, error) {
+	rows, err := r.pool.Query(ctx, getClickEventQuery,
+		linkID,
+		from,
+		to,
+	)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	stats := make([]analytics.DailyStat, 0)
+	for rows.Next() {
+		var stat analytics.DailyStat
+		if err := rows.Scan(&stat.Date, &stat.Count); err != nil {
+			return nil, err
+		}
+		stats = append(stats, stat)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return stats, nil
 }
